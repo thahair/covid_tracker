647185ff5a0d43d921fb6dedacae136186ced807
unknown <rsumanth4969@gmail.com>
2020-05-04 05:57:43 +0530

    Updates

---
+++ precache-manifest.e911f79303eb9517a05665505136cf13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f6ca9b0473f88780a77cad54481a858",
+    "url": "/covid_tracker/index.html"
+  },
+  {
+    "revision": "f9f8a983da6f179657c8",
+    "url": "/covid_tracker/static/css/main.47781929.chunk.css"
+  },
+  {
+    "revision": "26a0f6352febd21863ce",
+    "url": "/covid_tracker/static/js/2.ad6cb52b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid_tracker/static/js/2.ad6cb52b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9f8a983da6f179657c8",
+    "url": "/covid_tracker/static/js/main.51175bb9.chunk.js"
+  },
+  {
+    "revision": "6246dfcd5854f89a1e1c",
+    "url": "/covid_tracker/static/js/runtime-main.b423a1ff.js"
+  }
+]);
